7e00e21efe0315f8ce94b3c42cab37dfff998e00
nevarwin <ravencsolis@gmail.com>
2026-04-28 22:16:20 +0800

    Initial Commit

---
+++ MovieBox/MovieBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBoxApp.swift
+//  MovieBox
+//
+//  Created by raven on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
