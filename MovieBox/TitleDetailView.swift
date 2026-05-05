b38b990d32f0941ce5d711842dc7ddc803700c86
nevarwin <ravencsolis@gmail.com>
2026-05-05 22:08:58 +0800

    feat: create title detail view

---
+++ MovieBox/TitleDetailView.swift
@@ -0,0 +1,43 @@
+//
+//  TitleDetailView.swift
+//  MovieBox
+//
+//  Created by raven on 5/5/26.
+//
+
+import SwiftUI
+
+struct TitleDetailView: View {
+    let title: Title
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView {
+                LazyVStack(alignment: .leading) {
+                    AsyncImage(url: URL(string: title.posterPath ?? "")) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: geometry.size.width, height: geometry.size.height * 0.85)
+                    
+                    Text((title.name ?? title.title) ?? "")
+                        .bold()
+                        .font(.title2)
+                        .padding(5)
+                        .padding(.top, -20)
+                    
+                    Text(title.overview ?? "")
+                        .font(.body)
+                        .padding(5)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    TitleDetailView(title: Title.previewTitles[0])
+}
